15522341add1c34134964b5f5a48524950f99e6a
Alejandro Correa <smxacorreagmail.com>
2025-02-11 18:09:43 +0100

    Superado dia 2

--- src/quiz.js
+++ src/quiz.js
@@ -1,45 +1,59 @@
 class Quiz {
-    constructor(questions, timeLimit, timeRemaining) {
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+  }
+
+  shuffleQuestions() {
+    let indiceActual = this.questions.length,
+      temporal,
+      indiceAleatorio;
+
+    while (indiceActual !== 0) {
+      indiceAleatorio = Math.floor(Math.random() * indiceActual);
+      indiceActual--;
+
+      temporal = this.questions[indiceActual];
+      this.questions[indiceActual] = this.questions[indiceAleatorio];
+      this.questions[indiceAleatorio] = temporal;
     }
+  }
 
-    getQuestion() {
-        return this.questions[this.currentQuestionIndex];
+  checkAnswer(answer) {
+    if (this.questions[this.currentQuestionIndex].answer === answer)
+      this.correctAnswers += 1;
+  }
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
     }
+  }
 
-    moveToNextQuestion() {
-        this.currentQuestionIndex += 1;
-    }
+  filterQuestionsByDifficulty(dificultad) {
+    let listaQuestions = this.questions.filter((cadaQuestion) => {
+      return cadaQuestion.difficulty === dificultad;
+    });
+    if (dificultad >= 1 && dificultad <= 3) this.questions = listaQuestions;
+  }
 
-    shuffleQuestions() {
-        let indiceActual = this.questions.length, temporal, indiceAleatorio;
+  averageDifficulty() {
+    let averageResult = this.questions.reduce((acum, cadaQuestion) => {
+      return acum + cadaQuestion.difficulty;
+    }, 0);
 
-        while (indiceActual !== 0) {
-
-            indiceAleatorio = Math.floor(Math.random() * indiceActual);
-            indiceActual--;
-        
-            temporal = this.questions[indiceActual];
-            this.questions[indiceActual] = this.questions[indiceAleatorio];
-            this.questions[indiceAleatorio] = temporal;
-          }
-    }
-
-    checkAnswer(answer) {
-        if(this.questions[this.currentQuestionIndex].answer === answer)
-            this.correctAnswers += 1;
-
-    }
-    hasEnded() {
-        if(this.currentQuestionIndex < this.questions.length) {
-                return false;
-        } else if  (this.currentQuestionIndex === this.questions.length) {
-                return true;
-        }
-    }
+    return averageResult / this.questions.length;
+  }
 }
- 
